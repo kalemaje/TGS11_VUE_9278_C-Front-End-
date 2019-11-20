41b1b1a2d14e94af39a5fa204324f9aed66a1dfd
kalemaje <kalemajee@gmail.com>
2019-11-20 23:54:47 +0700

    my first commit

---
+++ src/components/dashboardContents/dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container>
+        <v-card>
+            <h1>
+                ini Dashboard
+            </h1>
+            <v-spacer></v-spacer>
+        </v-card>
+    </v-container>
+</template>
+            
--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,42 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 const DashboardLayout = () => import( /* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue')
-
+const loginLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/loginLayout.vue') 
 function loadView(view) {
     return () => import( /* webpackChunkName: "view[request]" */ `../components/dashboardContents/${view}.vue`)
 }
 
-const routes = [{
-    path: '/',
-    component: DashboardLayout,
-    children: [{
-        name: 'UserController',
-        path: '',
-        component: loadView('userController')
+
+const routes = [     
+    {       
+        path: '/dashboard',       
+        component: DashboardLayout,       
+        children: [         
+            {           
+                name: 'Dashboard',           
+                path: '/dashboard',           
+                component: loadView('dashboard')         
+            },
+            {           
+                name: 'UserController',           
+                path: '/userController',           
+                component: loadView('userController')         
+            },
+            {           
+                name: 'SparepartController',           
+                path: '/sparepartController',           
+                component: loadView('sparepartController')         
+            }        
+        ]     
     },
     {
-        path: '/sparepart',
-        name: 'sparepart',
-        component: loadView('sparepartCOntroller')
-    }
-    ]
-}, ] 
-Vue.use(Router)
-
-const router = new Router({
-    mode: 'history',
-    routes: routes
-})
-
-export default router
+        path: '/',
+        component: loginLayout,
+        name: 'loginLayout'
+    }   
+]   
+Vue.use(Router) 
+ 
+const router = new Router({mode: 'history', routes: routes}) 
+ 
+export default router   
